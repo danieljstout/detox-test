0b8ef3099fc80069c720a86fcf5101c9e7573d55
Micah Resnick <resnickmicah@gmail.com>
2023-11-05 19:33:53 -0500

    Fix faker name test

--- e2e/yourTestName.test.js
+++ e2e/yourTestName.test.js
@@ -24,10 +24,11 @@ describe('Example', () => {
   });
 
   it('shows "Hi, <name>!" when the user enters their name', async () => {
-    const name = faker.person.firstName;
+    const name = faker.person.firstName();
     const input = await element(by.id('name-input'));
     await input.clearText();
     await input.typeText(name);
+    await element(by.id('click-me-button')).tap();
 
     await expect(element(by.id('output-text'))).toHaveText(`Hi, ${name}!`);
   });
